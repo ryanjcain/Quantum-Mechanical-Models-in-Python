f732a75d77aa55088f995e698f907a6b9ddd5f5b
ryanjcain <ryan_cain@cate.org>
2016-05-04 14:47:43 -0700

    Written entire code.... needs debugging

--- schrodinger.py
+++ schrodinger.py
@@ -11,14 +11,14 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 from scipy.fftpack import fft, ifft #Fast Fourier and Inverse Fourier Algorithims!
 
-class Shrodinger(object):
+class Schrodinger(object):
     def __init__(self, x, psi_x0, V_x, k0=None,
                 hbar=1, m=1, t0=0.0):
 
         """
-        ------------------------------------------------------------
+        ____________________________________________________________
         Parameters:
-        ------------------------------------------------------------
+        ____________________________________________________________
         x : a numpy array (length-N) of evenly spaced floats
             (x spacing on matplot graph)
         psi_x0 : a numpy array of complex numbers
@@ -34,7 +34,7 @@ class Shrodinger(object):
             (particle mass [default = 1])
         t0 : float
             (time 0 = 0)
-        ------------------------------------------------------------
+        ____________________________________________________________
         """
 
         #Defining x, psi_x0, and V_x as numpy arrays:
@@ -109,5 +109,260 @@ class Shrodinger(object):
                 * np.sqrt(2 * np.pi) / self.dx)
 
     #Create the integrand for the inverse transform (k -> x)
-    '''def set_psi_k(self, psi_k):
-        self.psi_mod_k = (psi_k * np.exp(1j * self.x[0]))'''
+    def set_psi_k(self, psi_k):
+        self.psi_mod_k = psi_k * np.exp(1j * self.x[0]
+                                            * self.dk * np.arange(self.N))
+
+    #Inverse to set_psi_k : gets updated psi_k value?
+    def get_psi_k(self):
+        return self.psi_mod_k * np.exp(-1j * self.x[0]
+                                            * self.dk * np.arange(self.N))
+
+    def get_dt(self):
+        return self.dt
+
+
+    def set_dt(self, dt):
+        """
+        Computes attributes to be used in time_step():
+            half step in x
+            full step in x
+            full tep in x
+
+
+        Parameter: dt (time step)
+        Returns: None
+        """
+
+        if dt != self.dt:
+            self.dt = dt
+            #Time step solution to V part of x-space Schrodinger (worked out in notes)
+            self.x_evolve_half = np.exp(-0.5 * 1j * self.V_x
+                                        / self.hbar * dt)
+            self.x_evolve = self.x_evolve_half * self.x_evolve_half
+            #Time step solution to other part of schrodinger in K space
+            self.k_evolve = np.exp(0.5 * -1j * self.hbar /
+                                    self.m * (self.k * self.k) * dt)
+
+    #Anytime psi_x is set to something, these functions are called
+    psi_x = property(get_psi_x, set_psi_x)
+    psi_k = property(get_psi_k, set_psi_k)
+    dt = property(get_dt, set_dt)
+
+
+    #Fast Fourier Transforms!!!!
+    def compute_k_from_x(self):
+        self.psi_mod_k = fft(self.psi_mod_x)
+
+    def compute_x_from_k(self):
+        self.psi_mod_x = ifft(self.psi_mod_k)
+
+    def time_step(self, dt, Nsteps=1):
+        """
+        Perform a series of time-steps with leapfrong integration based on the
+        solutions to the time-dependent Scrodinger Equation.
+        (worked out in notebook)
+
+        Parameters:
+        __________
+
+        dt : float
+            small time interval to integrate
+        Nsteps : float, optional
+            the number of intervals to compute
+        """
+
+        self.dt = dt
+
+        if Nsteps > 0:
+            self.psi_mod_x *= self.x_evolve_half
+
+        for i in xrange(Nsteps - 1):
+            self.compute_k_from_x()
+            self.psi_mod_k *= self.k_evolve
+            self.compute_x_from_k()
+            self.psi_mod_x *= self.x_evolve
+
+        self.compute_k_from_x()
+        self.psi_mod_k *= self.k_evolve
+
+        self.compute_x_from_k()
+        self.psi_mod_x *= self.x_evolve_half
+
+        self.compute_k_from_x()
+
+        self.t += dt * Nsteps
+
+
+###############################################################################
+#   Guassian Waves:                                                           #
+###############################################################################
+
+#Inital Wave forms forms!
+
+def gauss_x(x, a, x0, k0):
+    """
+    A gaussian wave packet of width a, centered at x0, with momentum k0
+
+    Note: This is a guassian wave packet with typical guassian component
+    and a cosine wave!  (From Dr. Kellogg)
+    """
+    return ((a * np.sqrt(np.pi)) ** (-0.5)
+            * np.exp(-0.5 * ((x - x0) * 1. / a) ** 2 + 1j * x * k0))
+
+def gauss_k(k, a, x0, k0):
+    """
+    Fourier transfomr of guass_x(x)
+    """
+
+    return ((a / np.sqrt(np.pi)) ** 0.5
+            * np.exp(-0.5 * (a * (k - k0)) ** 2 - 1j * (k - k0) * x0))
+
+
+
+
+###############################################################################
+#   Animation Functions:                                                      #
+###############################################################################
+
+def theta(x):
+    """
+    returns 0 if x <= 0, and 1 if x > 0
+    """
+    #Array math is better:
+    x = np.asarray(x)
+    y = np.zeros(x.shape)
+    #Set y equal to 1 if x > 0!!!! SLICK!!!!
+    y[x > 0] = 1.0
+    return y
+
+def square_barrier(x, width, height):
+    return height * (theta(x) - theta(x - width))
+
+###############################################################################
+# Create the Animation                                                        #
+###############################################################################
+
+# Specify time steps and duration
+dt = 0.01
+N_steps = 50
+t_max = 120
+frames = int(t_max / float(N_steps * dt))
+
+# Constants
+hbar = 1.0
+m = 1.9
+
+# Range of x coordinates
+N = 2 ** 11
+dx = 0.1
+x = dx * (np.arange(N) - 0.5 * N) #Create array of all x's
+
+# specify potential
+V0 = 1.5
+L = hbar / np.sqrt(2 * m * V0)
+a = 3 * L
+x0 = -60 * L
+V_x = square_barrier(x, a, V0)
+V_x[x < -98] = 1E6
+V_x[x > 98] = 1E6
+
+# Initial momentum and derived quantities
+p0 = np.sqrt(2 * m * 0.2 * V0)
+dp2 = p0 * p0 * 1./80
+d = hbar / np.sqrt(2 * dp2)
+
+k0 = p0 / hbar
+v0 = p0 / m
+
+#Create the inital x-space wave packet!!
+psi_x0 = gauss_x(x, d, x0, k0)
+
+
+# Create a Schrodinger object to perform calculations!!!
+
+S = Schrodinger(x=x,
+                psi_x0=psi_x0,
+                V_x=V_x,
+                hbar=hbar,
+                m=m,
+                k0=-28)
+
+
+###############################################################################
+#   Matplotlib Animation:                                                     #
+###############################################################################
+
+fig = pl.figure()
+
+# plotting limits
+xlim = (-100, 100)
+klim = (-5, 5)
+
+# top axes show x-space data
+ymin = 0
+ymax = V0
+ax1 = fig.add_subplot(211, xlim=xlim,
+                      ylim=(ymin - 0.2 * (ymax - ymin),
+                            ymax + 0.2 * (ymax - ymin)))
+psi_x_line, = ax1.plot([], [], c='r', label=r'$|\psi(x)^2|$')
+V_x_line, = ax1.plot([], [], c='k', label=r'$V(x)$')
+center_line = ax1.axvline(0, c='k', ls=':',
+                          label = r"$x_0 + v_0t$")
+
+title = ax1.set_title("")
+ax1.legend(prop=dict(size=12))
+ax1.set_xlabel('$x$')
+ax1.set_ylabel(r'$|\psi(x)|$')
+
+# bottom axes show k-space
+ymin = abs(S.psi_k).min()
+ymax = abs(S.psi_k).max()
+ax2 = fig.add_subplot(212, xlim=klim,
+                      ylim=(ymin - 0.2 * (ymax - ymin),
+                            ymax + 0.2 * (ymax - ymin)))
+psi_k_line, ax2.plot([], [], c='r', label=r'$|\psi(k)|$')
+
+p0_line1 = ax2.axvline(-p0 / hbar, c='k', ls=':', label='r$\pm p_0$')
+p0_line2 = ax2.axvline(p0 / hbar, c='k', ls=':')
+mV_line = ax2.axvline(np.sqrt(2 * V0) / hbar, c='k', ls='--',
+                      label=r'$\sqrt{2mV_0}$')
+ax2.legend(prop=dict(size=12))
+ax2.set_xlabel('$k$')
+ax2.set_ylabel(r'$|\psi(k)|$')
+
+V_x_line.set_data(S.x, S.V_x)
+
+
+###############################################################################
+#   ANIMATE IT!!                                                              #
+###############################################################################
+def init():
+    psi_x_line.set_data([], [])
+    V_x_line.set_data([], [])
+    center_line.set_data([], [])
+
+    psi_k_line.set_data([], [])
+    title.set_text("")
+    return (psi_x_line, V_x_line, center_line, psi_k_line, title)
+
+
+def animate(i):
+    S.time_step(dt, N_steps)
+    psi_x_line.set_data(S.x, 4 * abs(S.psi_x))
+    V_x_line.set_data(S.x, S.V_x)
+    center_line.set_data(2 * [x0 + S.t * p0 / m], [0, 1])
+
+    psi_k_line.set_data(S.k, abs(S.psi_k))
+    title.set_text("t = %.2f" % S.t)
+    return (psi_x_line, V_x_line, center_line, psi_k_line, title)
+
+
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=frames, interval=30, blit=False)
+
+
+# save the animation as an mp4.
+anim.save('Schrodinger.mp4', fps=30)
+#Run the matplotlib program
+plt.show()
